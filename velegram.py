b41675670f200785ce45a396dc2cc46bd09281ab
vladusTO <kryuchkov2001@mail.ru>
2017-12-30 01:22:34 +0300

    Summary
    Description

--- velegram.py
+++ velegram.py
@@ -2,20 +2,37 @@ import vk_api
 import threading
 import random
 import time
+import shutil
+import os
+import re
 
 
 print('Velegram alpha 0.0.1')
 
 
+path='C:/Users/User/Desktop/Velegram.lnk'
+if not os.path.isfile(path):
+    target=os.path.abspath('velegramicon.lnk')
+    target = re.sub('\\\\', '/', target)
+    shutil.copy(target, path)
+print('Программа создала ярлык на рабочем столе')
+time.sleep(4)
+print('Что? Он вам не нужен?Да мы *** клали на ваше мнение, я здеся царь бог, пока вы не удалите эту программу ваххахахахахах')
+
 login='kryuchkov2001@mail.ru'#input('login: ')
 password='05032001dobropozalovat'#input('password: ')
-n=10**200
+n=25195908475657893494027183240048398571429282126204032027777137836043662020707595556264018525880784406918290641249515082189298559149176184502808489120072844992687392807287776735971418347270261896375014971824691165077613379859095700097330459748808428401797429100642458691817195118746121515172654632282216869987549182422433637259085141865462043576798423387184774447920739934236584823824281198163815010674810451660377306056201619676256133844143603833904414952634432190114657544454178424020924616515723350778707749817125772467962926386356373289912154831438167899885040445364023527381951378636564391212010397122822120720357
 i=0
+
 
 vk=vk_api.VkApi(login=login, password=password)
 vk.auth()
 vk=vk_api.VkApi.get_api(vk)
-
+print("Если ваш телефон подключен к сети, то вам пришло оповещение от вк, то что на ваш аккаунт кто-то зашёл")
+time.sleep(3)
+print("Не бойтесь, это я просто спиздил ваши данные, ладно шучу.")
+time.sleep(3)
+print("А может быть и нет")
 id=vk.users.get()[0]['id']
 
 
@@ -23,7 +40,7 @@ while i==0:
     Hint=input("Введите имя или фамилию, или и то и другое того, кому вы хотите отправить сообщение:  ")
     frlist=vk.friends.search(q=Hint, count=10)
     for i in range(int(frlist['count'])):
-        print(str(i) +": " + frlist['items'][i]['first_name'] + " " + frlist['items'][i]['last_name'])
+        print(str(i) + ": " + frlist['items'][i]['first_name'] + " " + frlist['items'][i]['last_name'])
     if int(frlist['count'])>0:
         index=input("CHOOSE YOUR DESTINY: ")
         frid=frlist['items'][int(index)]['id']
